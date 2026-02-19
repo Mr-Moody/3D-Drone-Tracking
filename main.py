8e46a71063088f3243487aad7a2ffeb78857214a
Mr-Moody <118393511+Mr-Moody@users.noreply.github.com>
2026-02-19 12:53:21 +0000

    Feat: Camera video streaming

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import cv2 as cv
 import numpy as np
+from pathlib import Path
 
 from src.window import Window
+from src.camera import Camera
+
+PATH = Path("drone-tracking-datasets")
 
 def main():
     window = Window()
-    image = np.linspace(0, 255, 256*256).reshape((256, 256)).astype(np.uint8)
-    window.show_image(image)
-    cv.waitKey(0)
+    camera = Camera(PATH / "videos1" / "cam0.mp4")
+    camera.load_from_file()
+    
+    while True:
+        frame = camera.get_frame()
+        
+        if frame is None:
+            break
+        
+        window.show_image(frame)
+        cv.waitKey(1000)
+        
     window.close()
+    camera.release()
 
 if __name__ == "__main__":
     main()
